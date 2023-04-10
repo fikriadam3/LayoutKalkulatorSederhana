3822e0853f124c40be5d55c0a926ac33b7515ad8
Fikri Adam Pratama S <fikriadam3@gmail.com>
2023-04-10 17:51:26 +0700

    Layout Kalkulator Sederhana

---
+++ app/src/main/java/com/example/layoutkalkulatorsederhana/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.layoutkalkulatorsederhana;
+//10120070
+//Fikri Adam Pratama Sutanto
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
